15f72091c7f28dc8a16b25dc0f3b8384b78acd02
Jake Gavin <jakegavin@gmail.com>
2016-10-06 19:27:39 -0700

    feat(Cards) basic card stack with swipe

---
+++ app/Card.js
@@ -0,0 +1,32 @@
+import React from "react"
+import { StyleSheet, Text, View } from "react-native"
+
+const styles = StyleSheet.create({
+  card: {
+    alignItems: "center",
+    backgroundColor: "#1f509e",
+    height: 150,
+    justifyContent: "center",
+    width: 150,
+  },
+  title: {
+    fontSize: 24,
+    color: "white",
+  }
+})
+
+export default class Card extends React.Component {
+  render() {
+    return (
+      <View style={styles.card} >
+        <Text style={styles.title}>
+          {this.props.title}
+        </Text>
+      </View>
+    )
+  }
+}
+
+Card.propTypes = {
+  title: React.PropTypes.string.isRequired,
+}
--- main.js
+++ main.js
@@ -1,28 +1,4 @@
-import Exponent from 'exponent';
-import React from 'react';
-import {
-  StyleSheet,
-  Text,
-  View,
-} from 'react-native';
-
-class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up main.js to start working on your app!</Text>
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+import Exponent from "exponent"
+import App from "./app/App"
 
 Exponent.registerRootComponent(App);
